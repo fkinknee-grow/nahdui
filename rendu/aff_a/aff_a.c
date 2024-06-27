b34b0c467920fc2c3d7c57351a998645f3270151
Mahdi <mahdi@example.com>
2024-06-27 17:11:02 -0500

    ]]

---
+++ rendu/aff_a/aff_a.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int main(int ac,char **av)
+{
+	int i;
+	i = 0;
+	if (ac == 2)
+	{
+		while(av[1][i])
+		{
+
+			if ((av[1][i]) == 'a')
+			{
+				write(1,&av[1][i],1);
+				write(1,"\n",1);
+				break;
+			}
+			i++;
+	    }
+
+	}
+	else
+		write(1,"\n",1);
+}

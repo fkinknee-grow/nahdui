1245dd8c7938d532682889fcf2a529c614c4a7a8
Mahdi <mahdi@example.com>
2024-06-27 19:21:52 -0500

    mahdu

--- rendu/aff_a/aff_a.c
+++ rendu/aff_a/aff_a.c
@@ -4,21 +4,27 @@ int main(int ac,char **av)
 {
 	int i;
 	i = 0;
-	if (ac == 2)
+	if(ac == 2)
 	{
 		while(av[1][i])
 		{
-
-			if ((av[1][i]) == 'a')
+			if (av[1][i] == 'a')
 			{
 				write(1,&av[1][i],1);
-				write(1,"\n",1);
 				break;
 			}
 			i++;
-	    }
-
+		}
+		write(1,"\n",1);
 	}
 	else
+		write(1,"a",1);
 		write(1,"\n",1);
+
+
+		
+
 }
+	
+		
+
